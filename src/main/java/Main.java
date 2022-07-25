cfd422bdeaa2e4e43860389d94423f656d4cc3a2
ArtJDev <artemkurilenko1983@yandex.ru>
2022-07-25 11:38:46 +0300

    Commit 25.07.2022

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,8 @@
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class Main {
     private static final int PORT = 9999;
-    private static final int THREADS = 64;
 
     public static void main(String[] args) {
-        final ExecutorService threadPool = Executors.newFixedThreadPool(THREADS);
-        Server server = new Server(threadPool);
+        Server server = new Server();
         server.serverStart(PORT);
     }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -6,15 +6,18 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
+    private final int THREADS = 64;
     private final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
     final ExecutorService threadPool;
 
-    public Server(ExecutorService threadPool) {
-        this.threadPool = threadPool;
+    public Server() {
+        this.threadPool = Executors.newFixedThreadPool(THREADS);
     }
 
     public void serverStart(int port) {
@@ -57,9 +60,25 @@ public class Server {
                                 "\r\n"
                 ).getBytes());
                 out.flush();
+            }
+            final Path filePath = Path.of(".", "public", path);
+            final String mimeType = Files.probeContentType(filePath);
+            if (path.equals("/classic.html")) {
+                final String template = Files.readString(filePath);
+                final byte[] content = template.replace(
+                        "{time}",
+                        LocalDateTime.now().toString()
+                ).getBytes();
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + content.length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.write(content);
+                out.flush();
             } else {
-                final Path filePath = Path.of(".", "public", path);
-                final String mimeType = Files.probeContentType(filePath);
                 final long length = Files.size(filePath);
                 out.write((
                         "HTTP/1.1 200 OK\r\n" +
